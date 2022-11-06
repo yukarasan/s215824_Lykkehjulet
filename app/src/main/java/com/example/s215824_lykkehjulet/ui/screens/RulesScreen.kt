07774c702c0e4f082b8cb0dc02b2b57dfb66c238
Yusuf Kara <yusufkarasan@hotmail.com>
2022-11-06 11:13:48 +0100

    RulesScreen is done.

--- app/src/main/java/com/example/s215824_lykkehjulet/ui/screens/RulesScreen.kt
+++ app/src/main/java/com/example/s215824_lykkehjulet/ui/screens/RulesScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -12,7 +11,6 @@ import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
